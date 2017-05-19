daa2dc1983a5fc8c54d6789e7c0276b276e16674
Estudiante <lian_rr@hotmail.com>
2017-05-19 14:12:47 -0600

    MongoDB library added

---
+++ src/java/com/una/progra/videoMDB/VideoMDB.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.una.progra.videoMDB;
+
+import com.mongodb.MongoClient;
+
+/**
+ *
+ * @author estudiante
+ */
+public class VideoMDB {
+    
+    
+    private final MongoClient mongo = new MongoClient("localhost",27017);
+    
+}
